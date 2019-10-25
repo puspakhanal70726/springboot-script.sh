2e3d53c1172e3a1edb95d0f8f2aa11393fcdba19
Administrator <khanalpuspa70726@gmail.com>
2019-10-24 22:24:06 -0400

    springboot-script

---
+++ springboot-script.sh
@@ -0,0 +1,13 @@
+   #!/bin/bash
+yum install python2 -y
+yum install java -y
+cd /usr/bin
+ln -s python2 python
+ln -s pip2 pip
+pip install awscli --upgrade --user
+export PATH=~/.local/bin:$PATH
+source ~/.bash_profile
+cd ~
+KEY=aws s3 ls s3://puspa --recursive |sort | tail -n 1 | awk '{print $4}'
+aws s3 cp s3://puspa/$KEY .
+java -jar $KEY
